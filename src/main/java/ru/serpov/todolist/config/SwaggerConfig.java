ff7775ca0a56cc04520813f2783164dc74551785
TimaSerp <timaserp@gmail.com>
2024-04-15 15:21:13 +0300

    add realization

---
+++ src/main/java/ru/serpov/todolist/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package ru.serpov.todolist.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ToDoList"
+        )
+)
+public class SwaggerConfig {
+}
